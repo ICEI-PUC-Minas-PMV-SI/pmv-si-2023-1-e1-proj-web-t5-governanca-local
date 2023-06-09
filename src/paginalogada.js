03cb097f5b02957e42c1a9626bba3ed39c88f4e5
PedroHenriqueNunes_ <pedro.alves.1460908@sga.pucminas.br>
2023-06-09 10:24:36 -0300

    Token

--- src/paginalogada.js
+++ src/paginalogada.js
@@ -3,7 +3,7 @@ let userlogado = JSON.parse(localStorage.getItem('userlogado'));
 let textologado = document.getElementById('textologado');
 textologado.innerHTML = 'Seja bem vindo, ' + userlogado.nome
 
-if (localStorage.getItem('token') == null) {
+if (localStorage.getItem('token' == null)) {
     alert("Vamos fazer login antes, amigão!");
     window.location.href = 'index.html'
 }
