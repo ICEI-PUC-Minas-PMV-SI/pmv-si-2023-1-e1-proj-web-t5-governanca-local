c25f9d27d5e87ea821754929130af7ef5494f99f
PedroHenriqueNunes_ <pedro.alves.1460908@sga.pucminas.br>
2023-06-09 20:07:22 -0300

    Organização

--- src/js/paginalogada.js
+++ src/js/paginalogada.js
@@ -3,7 +3,7 @@ let userlogado = JSON.parse(localStorage.getItem('userlogado'));
 
 if (localStorage.getItem('token') == null) {
     alert("Vamos fazer login antes, amigão!");
-    window.location.href = 'index.html'
+    window.location.href = '../index.html'
 }
 
 let textologado = document.getElementById('textologado');
