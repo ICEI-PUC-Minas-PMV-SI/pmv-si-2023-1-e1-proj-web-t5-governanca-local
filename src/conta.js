2a0d13c92f5e29cb690a88ed6f675a42619d5d89
PedroHenriqueNunes_ <pedro.alves.1460908@sga.pucminas.br>
2023-06-09 18:02:42 -0300

    API de Puxar CEP

---
+++ src/conta.js
@@ -0,0 +1,26 @@
+const cep = document.getElementById('cep');
+const rua = document.getElementById('rua');
+const bairro = document.getElementById('bairro');
+const cidade = document.getElementById('cidade');
+const estado = document.getElementById('estado');
+const span = document.getElementById('bairroflex');
+
+cep.addEventListener('focusout', async () =>{
+
+    const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+
+    if(!response.ok){
+        throw await response.json();
+    }
+
+    const responseCep = await response.json();
+
+    rua.value = responseCep.logradouro;
+    bairro.value = responseCep.bairro;
+    span.value = responseCep.bairro;
+    cidade.value = responseCep.localidade;
+    estado.value = responseCep.uf;
+
+    span.innerHTML = responseCep.bairro;
+
+})
