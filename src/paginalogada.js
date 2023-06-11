c26d1c953fee00fdadfa548cf25b681adf87905a
PedroHenriqueNunes_ <pedro.alves.1460908@sga.pucminas.br>
2023-06-11 15:44:35 -0300

    Pg não autenticação

--- src/paginalogada.js
+++ src/paginalogada.js
@@ -2,8 +2,7 @@ let userlogado = JSON.parse(localStorage.getItem('userlogado'));
 
 
 if (localStorage.getItem('token') == null) {
-    alert("Vamos fazer login antes, amigão!");
-    window.location.href = '../index.html'
+    window.location.href = 'aviso.html'
 }
 
 let textologado = document.getElementById('textologado');
