96d0c7e577b5f0280f818db2bb71a020de5cb387
PedroHenriqueNunes_ <pedro.alves.1460908@sga.pucminas.br>
2023-06-17 17:55:27 -0300

    Início do fórum

---
+++ src/js/forum.js
@@ -0,0 +1,34 @@
+let textarea = document.getElementById('sendcoment');
+
+
+function enviarComentario(){
+if (textarea.value == "") {
+    alert("Digite algo antes de enviar!")
+} else {
+    let comentarioUser = JSON.parse(localStorage.getItem('comentarioUser') || '[]')
+
+    comentarioUser.push ({
+        mensagem: textarea.value
+    });
+    
+    localStorage.setItem('comentarioUser', JSON.stringify(comentarioUser));   
+    
+}
+}
+
+var mensagemSalva = JSON.parse(localStorage.getItem('comentarioUser'));
+let userlogado = JSON.parse(localStorage.getItem('userlogado'));
+var mensagem = mensagemSalva[0].mensagem;
+var mensagem2 = mensagemSalva[1].mensagem;
+
+let campo = document.getElementById('coment');
+let pessoa = document.getElementById('pessoa');
+campo.innerHTML = mensagem;
+pessoa.innerHTML = userlogado.nome;
+
+// Fazer i++ dos comentários
+
+let campo2 = document.getElementById('comentario2');
+campo2.innerHTML = mensagem2;
+
+
